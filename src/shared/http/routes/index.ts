dfbc5a9eef124b7eee848923c6ad4701c0370155
Natanael <natanaelsilva.progjs@gmail.com>
2021-08-11 23:56:01 -0300

    Feat: Creation routes of products

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,9 @@
 import { Request, Response, Router } from 'express';
+import productRouter from '@modules/products/routes/products.routes';
 
 const routes = Router();
+
+routes.use('/products', productRouter);
 
 routes.get('/', (req: Request, res: Response) => {
   return res.json({ message: 'Hello dev' });
