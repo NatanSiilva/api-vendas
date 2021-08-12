8bcd7dc86da46a55c76fe9a581800a38413c9d35
Natanael <natanaelsilva.progjs@gmail.com>
2021-08-11 23:31:35 -0300

    feat: creation controller of product

---
+++ src/modules/products/services/DeleteProductService.ts
@@ -0,0 +1,19 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
+
+class DeleteProductService {
+  public async execute(id: string): Promise<void> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found');
+    }
+
+    await productsRepository.remove(product);
+  }
+}
+
+export default DeleteProductService;
