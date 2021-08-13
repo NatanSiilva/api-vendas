9439f5145b21fd3cb95535f41c415a902be6cc3c
Natanael <natanaelsilva.progjs@gmail.com>
2021-08-12 22:31:37 -0300

    Feat: Creation routes add validate data of user

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Request, Response, Router } from 'express';
 import productRouter from '@modules/products/routes/products.routes';
+import usersRouter from '@modules/users/routes/users.routes';
 
 const routes = Router();
 
 routes.use('/products', productRouter);
+routes.use('/users', usersRouter);
 
 routes.get('/', (req: Request, res: Response) => {
   return res.json({ message: 'Hello dev' });
