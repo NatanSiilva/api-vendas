b7ce21c6e80e68aca213f5408b258d8cc6eb7ee1
Natanael <natanaelsilva.progjs@gmail.com>
2021-08-12 23:48:25 -0300

    Feat: Creation service of user

---
+++ src/modules/users/services/CreateSessionsService.ts
@@ -0,0 +1,50 @@
+import AppError from '@shared/errors/AppError';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: User;
+  token: string;
+}
+
+class CreateSessionsService {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    const passwordConfirmed = await compare(password, user.password);
+
+    if (!passwordConfirmed) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    const token = sign(
+      { name: user.name, id: user.id },
+      '8a5da52ed126447d359e70c05721a8aa',
+      {
+        subject: user.id,
+        expiresIn: '1d',
+      },
+    );
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default CreateSessionsService;
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Request, Response, Router } from 'express';
 import productRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
+import sessionsRouter from '@modules/users/routes/sessions.routes';
 
 const routes = Router();
 
+routes.use('/sessions', sessionsRouter);
 routes.use('/products', productRouter);
 routes.use('/users', usersRouter);
 
