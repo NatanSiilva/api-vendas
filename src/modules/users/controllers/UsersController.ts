25273e624a7d791d87cbf0bc31001ea7d2f40ff8
Natanael <natanaelsilva.progjs@gmail.com>
2021-08-21 23:11:18 -0300

    feat: controller update profile

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -1,22 +1,12 @@
 import { Request, Response } from 'express';
 import CreateUserService from '../services/CreateUserService';
 import ListUserService from '../services/ListUserService';
-import ShowProfileService from '../services/ShowProfileService';
 
 export default class UsersController {
   public async index(req: Request, res: Response): Promise<Response> {
     const listUsers = new ListUserService();
 
     const users = await listUsers.execute();
-
-    return res.json(users);
-  }
-
-  public async show(req: Request, res: Response): Promise<Response> {
-    const { id } = req.params;
-    const showUsers = new ShowProfileService();
-
-    const users = await showUsers.execute(id);
 
     return res.json(users);
   }
